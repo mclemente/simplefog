1ab69bd14ebee4a1c8ad3526e5eb541cf18a1cd5
Matheus Clemente <matheus.xb@gmail.com>
2025-06-11 14:45:27 -0300

    Add settings localization

--- src/module/settings.js
+++ src/module/settings.js
@@ -1,40 +1,40 @@
 export const registerSettings = function () {
 	// Register global config settings
 	game.settings.register("simplefog", "confirmFogDisable", {
-		name: "Confirm Disabling of Scene Simple Fog",
-		hint: "When enabled, a confirmation dialog will be displayed before Simple Fog can be toggled off for a scene",
+		name: "SIMPLEFOG.SETTINGS.confirmFogDisable.name",
+		hint: "SIMPLEFOG.SETTINGS.confirmFogDisable.hint",
 		scope: "world",
 		config: true,
 		default: true,
 		type: Boolean,
 	});
 	game.settings.register("simplefog", "autoEnableSceneFog", {
-		name: "Auto Enable Scene Fog",
-		hint: "When enabled, Simple Fog will automatically be enabled for a scene when it is first created.",
+		name: "SIMPLEFOG.SETTINGS.autoEnableSceneFog.name",
+		hint: "SIMPLEFOG.SETTINGS.autoEnableSceneFog.hint",
 		scope: "world",
 		config: true,
 		default: true,
 		type: Boolean,
 	});
 	game.settings.register("simplefog", "toolHotKeys", {
-		name: "Hotkey Tool",
-		hint: "Define which tool will be selected when using the keybinding.",
+		name: "SIMPLEFOG.SETTINGS.toolHotKeys.name",
+		hint: "SIMPLEFOG.SETTINGS.toolHotKeys.hint",
 		scope: "world",
 		config: true,
 		default: "brush",
 		type: String,
 		choices: {
-			brush: "Brush",
-			grid: "Grid",
-			room: "Room",
-			polygon: "Polygon",
-			box: "Box",
-			ellipse: "Ellipse",
+			brush: "SIMPLEFOG.SETTINGS.toolHotKeys.choices.brush",
+			grid: "SIMPLEFOG.SETTINGS.toolHotKeys.choices.grid",
+			room: "SIMPLEFOG.SETTINGS.toolHotKeys.choices.room",
+			polygon: "SIMPLEFOG.SETTINGS.toolHotKeys.choices.polygon",
+			box: "SIMPLEFOG.SETTINGS.toolHotKeys.choices.box",
+			ellipse: "SIMPLEFOG.SETTINGS.toolHotKeys.choices.ellipse",
 		},
 	});
 	game.settings.register("simplefog", "zIndex", {
-		name: "Simplefog Z-Index",
-		hint: "The z-index determines the order in which various layers are rendered within the Foundry canvas. A higher number will be rendered on top of lower numbered layers (and the objects on that layer). This allows for the adjustment of the z-index to allow for Simple Fog to be rendered above/below other layers; particularly ones added by other modules. Going below 200 will intermingle with Foundry layers such as the foreground image (200), tokens (100), etc... (Default: 220)",
+		name: "SIMPLEFOG.SETTINGS.zIndex.name",
+		hint: "SIMPLEFOG.SETTINGS.zIndex.hint",
 		scope: "world",
 		config: true,
 		default: 220,
