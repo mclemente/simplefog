9d4ae162ab6f077e74d055fd623caffba9d1d260
Matheus Clemente <matheus.xb@gmail.com>
2025-06-11 14:54:00 -0300

    Brush Controls fixes

--- src/module/apps/BrushControls.js
+++ src/module/apps/BrushControls.js
@@ -52,8 +52,8 @@ export class BrushControls extends HandlebarsApplicationMixin(ApplicationV2) {
 	/** @inheritDoc */
 	_onFirstRender(context, options) {
 		super._onFirstRender(context, options);
-		const left = ui.nav?.position.left;
-		const top = ui.controls?.position.top;
+		const left = ui.nav?.element.getBoundingClientRect().left;
+		const top = ui.controls?.element.getBoundingClientRect().top;
 		options.position = {...options.position, left, top};
 		canvas.scene.apps[this.id] = this;
 	}
