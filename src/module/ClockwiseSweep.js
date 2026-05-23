a6a1b3178094c0571ee26af089428d233e963ee0
Matheus Clemente <matheus.xb@gmail.com>
2026-05-23 04:46:36 -0300

    V14 update
    Deprecated `wallDirectionMode` for `edgeDirectionMode`
    Moved logic around to match V14

--- src/module/ClockwiseSweep.js
+++ src/module/ClockwiseSweep.js
@@ -2,7 +2,7 @@
 
 export class CWSPNoDoors extends foundry.canvas.geometry.ClockwiseSweepPolygon {
 	_testEdgeInclusion(edge, edgeTypes) {
-		const { type, boundaryShapes, useThreshold, wallDirectionMode, externalRadius } = this.config;
+		const { type, boundaryShapes, useThreshold, edgeDirectionMode, externalRadius } = this.config;
 
 		// Only include edges of the appropriate type
 		const edgeType = edgeTypes[edge.type];
@@ -13,11 +13,6 @@ export class CWSPNoDoors extends foundry.canvas.geometry.ClockwiseSweepPolygon {
 		// Exclude edges with a lower priority than required for this polygon
 		if (edge.priority < edgeType.priority) return false;
 
-		// Specific boundary shapes may impose additional requirements
-		for (const shape of boundaryShapes) {
-			if (shape._includeEdge && !shape._includeEdge(edge.a, edge.b)) return false;
-		}
-
 		// Ignore edges which do not block this polygon type
 		const isOpenDoor = edge.type === "wall" && edge.object.isOpen;
 		if ((this.config.shiftKey || !isOpenDoor) && edge[type] === CONST.EDGE_SENSE_TYPES.NONE) return false;
@@ -27,13 +22,19 @@ export class CWSPNoDoors extends foundry.canvas.geometry.ClockwiseSweepPolygon {
 		if (!side) return false;
 
 		// Ignore one-directional walls which are facing away from the origin
-		const wdm = CONST.EDGE_DIRECTION_MODES;
-		if (edge.direction && (wallDirectionMode !== wdm.BOTH)) {
-			if ((wallDirectionMode === wdm.NORMAL) === (side === edge.direction)) return false;
+		const edm = CONST.EDGE_DIRECTION_MODES;
+		if (edge.direction && (edgeDirectionMode !== edm.BOTH)) {
+			if ((edgeDirectionMode === edm.NORMAL) === (side === edge.direction)) return false;
 		}
 
 		// Ignore threshold walls which do not satisfy their required proximity
-		if (useThreshold) return !edge.applyThreshold(type, this.origin, externalRadius);
+		if (useThreshold && edge.applyThreshold(type, this.origin, externalRadius)) return false;
+
+		// Specific boundary shapes may impose additional requirements
+		for (const shape of boundaryShapes) {
+			if (shape._includeEdge && !shape._includeEdge(edge.a, edge.b)) return false;
+		}
+
 		return true;
 	}
 }
